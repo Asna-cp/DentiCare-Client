ce84d53370a91378968faaffec47c1101b6806fb
asn710 <cpasnaasn@gmail.com>
2023-02-27 17:45:18 +0530

    profile page style

--- src/pages/Profile/profile.jsx
+++ src/pages/Profile/profile.jsx
@@ -24,9 +24,9 @@ const Profile = () => {
     <div>
       <Navbar />
       <div class="w-full">
-        <div class="bg-gradient-to-b from-blue-800 to-blue-600 h-96"></div>
-        <div class="max-w-5xl mx-auto px-6 sm:px-6 lg:px-8 mb-12">
-          <div class="bg-white w-full shadow rounded p-8 sm:p-12 -mt-72">
+        <div class="bg-gradient-to-b from-blue-800 to-blue-600 h-20"></div>
+        <div class="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 mb-12">
+          <div class="bg-white w-full shadow rounded p-8 sm:p-12 -mt-19">
             <p class="text-3xl font-bold leading-7 text-center">Your Profile</p>
             <form action="" method="post">
               <div class="md:flex items-center mt-12">
