e07563d0ce188bd5110b15282c02035e883400d5
asn710 <cpasnaasn@gmail.com>
2023-03-07 19:09:50 +0530

    navigate

--- src/pages/Doctors/doctors.jsx
+++ src/pages/Doctors/doctors.jsx
@@ -20,7 +20,9 @@ const Doctors = () => {
   return (
     <div>
       <Navbar />
-      <Link className="grid grid-cols-12 p-12">
+      <Link
+      to= {"/doctorDetails"}
+      className="grid grid-cols-12 p-12">
         {doctors?.map((doctor) => (
           <div class="rounded-lg shadow-lg bg-white m-3 col-span-4">
             
